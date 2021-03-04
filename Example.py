831a58644b350d878856fc6b6994986c6c241e95
jonasgrs <jonasweb@posteo.net>
2021-03-04 12:36:11 +0100

    Add Example.py to show the functioning of the OpenDHW method.

---
+++ Example.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import OpenDHWcalc
+
+# ------- Parameter Section ---------
+s_step = 60
+start_plot = '2019-03-01'
+end_plot = '2019-03-08'
+
+
+def main():
+
+    # generate time-series with OpenDHWcalc
+    x, water_open_dhw_60 = OpenDHWcalc.generate_dhw_profile_open_dhwcalc(
+        s_step=s_step
+    )
+
+    # Load time-series from DHWcalc
+    x, water_dhwcalc_60 = OpenDHWcalc.import_from_dhwcalc(s_step=s_step)
+
+    # compare the two time-series for a specific time-period
+    OpenDHWcalc.compare_generators(
+        first_method='DHWcalc',
+        first_series_LperH=water_dhwcalc_60,
+        second_method='OpenDHW',
+        second_series_LperH=water_open_dhw_60,
+        s_step=s_step,
+        start_plot=start_plot,
+        end_plot=end_plot,
+    )
+
+
+if __name__ == '__main__':
+    main()
