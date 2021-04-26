bfaacb16367fdaf8652291653660c97254a19c89
jonasgrs <jonasweb@posteo.net>
2021-04-26 11:49:12 +0200

    Add files with different mean drawoff volumes per day for DHWcalc. This ensures new random seeds, not different max flow rates.

---
+++ Examples/Example2_DHWcalc_mean_flow_rates.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import OpenDHW
+import pandas as pd
+
+"""
+This Example loads multiple TimeSeries from DHWcalc for one category, 
+with different mean daily drawoff volumes.
+
+One can see that the adjusted mean daily drawoff volumes results in new seeds
+for the generation and distribution of the drawoff event.
+"""
+
+# --- Parameters ---
+start_plot = '2019-03-04-06'
+end_plot = '2019-03-04-10'
+
+# --- Constants ---
+s_steps = [60, 900, 3600]
+mean_drawoff_vols_per_day = [197, 198, 199, 200, 201, 202, 203]
+categories = 1
+
+
+def main():
+
+    for s_step in s_steps:
+
+        timeseries_lst = []
+        drawoffs_lst = []
+
+        # for each chosen mean drawoff volume, import the DHWcalc Timeseries.
+        for mean_drawoff_vol_per_day in mean_drawoff_vols_per_day:
+
+            # Load time-series from DHWcalc
+            dhwcalc_df = OpenDHW.import_from_dhwcalc(
+                s_step=s_step,
+                daylight_saving=False,
+                categories=categories,
+                mean_drawoff_vol_per_day=mean_drawoff_vol_per_day
+            )
+
+            timeseries_lst.append(dhwcalc_df)
+            drawoffs_lst.append(OpenDHW.get_drawoffs(dhwcalc_df))
+
+        # plot all generated timeseries
+        OpenDHW.plot_multiple_timeseries(
+            timeseries_lst=timeseries_lst,
+            start_plot=start_plot,
+            end_plot=end_plot,
+            plot_hist=False
+        )
+
+        pass
+
+
+if __name__ == '__main__':
+    main()
