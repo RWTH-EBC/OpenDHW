0b3ca22aa93c8756dae9bb6a687b12b81f5a1dbd
Carla Wüller <carla.wueller@eonerc.rwth-aachen.de>
2025-02-20 15:00:54 +0100

    [PYPI-RELEASE]  version 0.2.2

--- OpenDHW/__init__.py
+++ OpenDHW/__init__.py
@@ -4,3 +4,5 @@ This package contains the script that stores all function of the DHWcalc package
 as well as a utils package.
 """
 
+from OpenDHW.OpenDHW import *
+__version__ = '0.2.2'
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+
+
+with open(Path(__file__).parent.joinpath("OpenDHW", "__init__.py"), "r") as file:
+    for line in file.readlines():
+        if line.startswith("__version__"):
+            VERSION = line.replace("__version__", "").split("=")[1].strip().replace("'", "").replace('"', '')
+
 setup(
     name="OpenDHW",
-    version="0.2.1",
+    version=VERSION,
     description="Tool for generating domestic hot water profiles",
     url="https://github.com/RWTH-EBC/OpenDHW",
     author="RWTH Aachen University, E.ON Energy Research Center, "
@@ -14,5 +23,8 @@ setup(
         "OpenDHW.Data",
         "OpenDHW.utils"
     ],
+    package_data={
+        "OpenDHW.Data": ["*.json"]
+    },
     install_requires=["scipy", "pathlib", "pandas", "numpy", "matplotlib", "seaborn", "holidays"],
 )
