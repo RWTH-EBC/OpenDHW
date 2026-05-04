bc105b04dd8eac26c2a32838f5852b49bb053754
Boran Kaya <kayaboran71@gmail.com>
2026-05-04 21:02:50 +0200

    Added basic occupancy profile logic

---
+++ build/lib/OpenDHW/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+This package contains the script that stores all function of the DHWcalc package
+as well as a utils package.
+"""
+
+from OpenDHW.OpenDHW import *
+__version__ = '0.2.8'
